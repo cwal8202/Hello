51689076977516f3752fba0be51030912604a8b1
Administrator <Administrator@SC-202106161522>
2022-06-29 10:20:21 +0900

    푸쉬 연습중~

--- 220629-practice/src/Main.java
+++ 220629-practice/src/Main.java
@@ -3,5 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 		// helloworld 할 것이다.
 		System.out.println("Hello World");
+		System.out.println("Hello World 2");
+		// push 연습
+		System.out.println("연습중");
 	}
 }
